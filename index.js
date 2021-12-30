40ec400eebfd6d9d653724fe0e8d102d26112cb5
PrieurA <alexandre-prieur_student2019@wilder.school>
2021-12-30 23:44:25 +0100

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.login(process.env.TOKEN)
 client.on('ready', () => {
   console.log(`Bot Open Sea has started.`)
   let getFloorPrice = async () => {
-    let res = await axios.get('https://api.opensea.io/api/v1/collection/exclusible-gold-alpha/stats');
+    let res = await axios.get('https://api.opensea.io/api/v1/collection/exclusible-alpha/stats');
     console.log(res.data.stats.floor_price);
     client.user.setActivity(res.data.stats.floor_price + ' Ξ ', { type: 'WATCHING' })
   }
